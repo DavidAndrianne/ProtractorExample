0774caab46eafdc99cc0124a94656038354a5d9e
DavidAndrianne <r0426942@gmail.com>
2016-11-20 22:31:41 +0100

    init

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/debug/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/ms/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
